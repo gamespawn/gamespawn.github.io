d13bb9c3c88ade704645ceafc3a949816702b9da
Brandon Gutzmann <bgutz001@ucr.edu>
2017-01-22 13:16:12 -0800

    Projects: Finished making the design for the Checkboxes. Also gave the checkboxes functionality. Checking a checkbox will make only the projects with tha tag show. Added no results found if no projects with the selected tags exist.
    System: Moved unused code into a directory call Unused.
    
    Bugs: No results found is not displaying correctly.

--- Projects/projects.js
+++ Projects/projects.js
@@ -1,65 +1,51 @@
-var MAX_PTEXT_LENGTH = 1250;
+var tagsMap = {};
 
-
-function start() {
-    // list of all the project-text items
-    var projects_text_list = document.getElementsByClassName("projects-text");
-
-    // lists containing cut and original versions of project-text text
-    var long_content = [];
-    var short_content = [];
-
-    
-    // Split every paragraph with class name projects-text if it exceeds max length
+function updateProjects() {
     var i;
-    for (i = 0; i < projects_text_list.length; i++) {
-	var paragraphs = projects_text_list[i].getElementsByTagName('p');
-	var current_text = paragraphs[0].innerHTML;
-	long_content.push(current_text);
-	short_content.push(current_text);
-
-	if (current_text.length > MAX_PTEXT_LENGTH) {
-	    // Cut off paragraph at the end of a sentence.
-	    var j;
-	    for (j = MAX_PTEXT_LENGTH; j < current_text.length; j++) {
-		if (current_text.charAt(j) == '.') {
-		    short_content[i] = current_text.substr(0, j+1);
-		    break;
-		    // TODO: handle when no period is found
-		}
+    
+    for (i = 0; i < start.liList.length; ++i) {
+	var tags = start.liList[i].getElementsByClassName("projects-tags")[0].innerHTML.toString().split(", ");
+	var j;
+	for (j = 0; j < tags.length; ++j) {
+	    if (tagsMap[tags[j].trim()] == true) {
+		start.liList[i].style.display = "";
+		break;
 	    }
-	    // Set the content of the paragraph	    
-	    paragraphs[0].innerHTML = short_content[i];
-
-
-	    // Add a read more button
-	    var button = document.createElement("button");
-	    button.name = i;
-	    button.innerHTML = "Read More";
-	    button.className = "read_more_less";
-	    button.onclick = function() {
-		if (this.innerHTML == "Show Less") {
-		    projects_text_list[this.name].getElementsByTagName('p')[0].innerHTML =
-			short_content[this.name];
-		    this.innerHTML = "Read More";
-
-		    // Scroll so the top of the list item is at the top of the screen
-		    var liTop = this.parentElement.parentElement.offsetTop;
-		    var h1Height = this.parentElement.parentElement.parentElement.getElementsByTagName('h1')[0].offsetHeight;
-		    var navBarHeight = document.getElementById("navBar").offsetHeight;
-		    
-		    window.scrollTo(0, liTop - h1Height - navBarHeight - 50);
-		}
-		else {
-		    projects_text_list[this.name].getElementsByTagName('p')[0].innerHTML =
-			long_content[this.name];
-		    this.innerHTML = "Show Less";
-		}
-	    };
-	    projects_text_list[i].appendChild(button)
-	    
-							 
+	    else {
+		start.liList[i].style.display = "none";
+	    }
 	}
     }
+}
+
+function toggleCheckbox(element) {
+    if (element.checked) {
+	tagsMap[element.id.toString()] = true;
+    }
+    else {
+	tagsMap[element.id.toString()] = false;
+    }
+
+    updateProjects();
+
+    for (var i in tagsMap) {
+	if (tagsMap[i] == true) {
+	    return;
+	}
+    }
+
+    var i;
+    for (i=0; i < start.liList.length; ++i) {
+	start.liList[i].style.display = "";
+    }
+    
+}
+
+function start() {
+    console.log("started");
+    
+    start.liList = document.getElementsByClassName("projects");
+    start.liList = start.liList[0].getElementsByTagName("UL");
+    start.liList = start.liList[0].getElementsByTagName("LI");
     
 }
