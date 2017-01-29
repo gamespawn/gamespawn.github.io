f1682ae56b86dfd8a15be4edca3507c3ce72bcb0
Brandon Gutzmann <bgutz001@ucr.edu>
2017-01-29 15:50:55 -0800

    Projects: Reduced the font size of multiple items. Project items that are outside the viewing bounderies are no longer visible.
    Bugs: Still need to fix the no results found item
    
    TODO: Add slideshow to top of the page. Add a way to scroll through pages of projects. Add method to hide excessive project text.

--- Projects/projects.js
+++ Projects/projects.js
@@ -2,10 +2,11 @@ var tagsMap = {};
 
 function updateProjects() {
     var i;
-    
     for (i = 0; i < start.liList.length; ++i) {
 	var tags = start.liList[i].getElementsByClassName("projects-tags")[0].innerHTML.toString().split(", ");
 	var j;
+
+	// Check if the item has a tag that is checked
 	for (j = 0; j < tags.length; ++j) {
 	    if (tagsMap[tags[j].trim()] == true) {
 		start.liList[i].style.display = "";
@@ -15,8 +16,37 @@ function updateProjects() {
 		start.liList[i].style.display = "none";
 	    }
 	}
+
+    }
+
+    // Stores if all the checkboxes are unchecked
+    var emptyMap = true;
+    for (var i in tagsMap) {
+	if (tagsMap[i] == true) {
+	    emptyMap = false;
+	    break;
+	}
+    }
+
+    // Since we have an empty map, display all the items
+    if (emptyMap == true) {
+	console.log("All checkboxes unchecked");
+	for (i=0; i < start.liList.length; ++i) {
+	    start.liList[i].style.display = "";
+	}
+    }
+
+    // Check to see if the items are off the screen
+    // and hide them if they are
+    for (i = 0; i < start.liList.length; ++i) {
+	var bottom = start.liList[i].offsetTop + start.liList[i].offsetHeight;
+	if (bottom > start.height) {
+	    console.log("cutting " + bottom + " " + start.height);
+	    start.liList[i].style.display = "none";
+	}
     }
 }
+
 
 function toggleCheckbox(element) {
     if (element.checked) {
@@ -27,25 +57,23 @@ function toggleCheckbox(element) {
     }
 
     updateProjects();
-
-    for (var i in tagsMap) {
-	if (tagsMap[i] == true) {
-	    return;
-	}
-    }
-
-    var i;
-    for (i=0; i < start.liList.length; ++i) {
-	start.liList[i].style.display = "";
-    }
-    
 }
 
 function start() {
     console.log("started");
-    
+
+    // Get every project list item
+    start.height = window.innerHeight;
     start.liList = document.getElementsByClassName("projects");
     start.liList = start.liList[0].getElementsByTagName("UL");
     start.liList = start.liList[0].getElementsByTagName("LI");
-    
+
+    // Get every input item and uncheck it
+    // TODO: Make sure we only get checkboxes
+    var checkboxes = document.getElementsByTagName("INPUT");
+    for (var i in checkboxes) {
+	checkboxes[i].checked = false;
+    }
+
+    updateProjects();
 }
