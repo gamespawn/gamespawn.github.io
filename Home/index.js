923f6b7bab23b4f32dbb1597b6dbccec57b02e5d
Michaella Sheng <msheng@ucsd.edu>
2017-02-05 16:02:11 -0800

    Started Dynamic Div Resizing

--- Home/index.js
+++ Home/index.js
@@ -1,36 +1,63 @@
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
-}
 var slideIndex = 1;
-window.onload=function() {
-    showSlides(slideIndex);
+function showSlides(n) {
+    "use strict";
+    var i,
+        slides = document.getElementsByClassName("mySlides"),
+        dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i += 1) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i += 1) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+function plusSlides(n) {
+    "use strict";
+    showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+    "use strict";
+    showSlides(slideIndex = n);
 }
 
-//function GetContainerSize ()
-//{
-//    var textContainer = document.getElementById ("DescriptionText");
-//    var imgContainer = document.getElemnentById ("")
-//    while( is_overflow_in_the_second_div() ) {
-//        set_smaller_font();
-//    }
-//}
+function isOverflowed(element) {
+    "use strict";
+    return element.scrollHeight > element.clientHeight || element.scrollWidth > element.clientWidth;
+}
+function setSmallerFont(element) {
+    "use strict";
+    //$('#fitin div').css('font-size', (parseInt($('#fitin div').css('font-size')) - 1) + "px" );
+    return 0;
+}
+function GetContainerSize() {
+    "use strict";
+    var wText = document.getElementById("DescriptionText1"),
+        pText = document.getElementById("DescriptionText2"),
+        eText = document.getElementById("DescriptionText3"),
+        sText = document.getElementById("DescriptionText4");
+    if (isOverflowed(wText)) {
+        setSmallerFont(wText);
+    }
+    if (isOverflowed(pText)) {
+        setSmallerFont(pText);
+    }
+    if (isOverflowed(eText)) {
+        setSmallerFont(eText);
+    }
+    if (isOverflowed(sText)) {
+        setSmallerFont(sText);
+    }
+}
+window.onload = function () {
+    "use strict";
+    showSlides(slideIndex);
+};
+
