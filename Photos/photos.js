5fde341ac25df4f21c8528617f6d5b0f33c78a4f
qnguy019 <qnguy019@ucr.edu>
2017-03-05 16:18:44 -0800

    Added thumbnail carousel effect

--- Photos/photos.js
+++ Photos/photos.js
@@ -1,15 +1,33 @@
 function deleteList(){
-  var myList = document.getElementById('image_list');
-  myList.innerHTML = '';
+  var temp = 1;
+  while (temp <= thumbnailSetMaxNumber)
+  {
+    var name = "image_list" + temp;
+    var myList = document.getElementById(name);
+    myList.innerHTML = '';
+    temp++;
+  }
 }
 function viewImage(path){
   document.getElementById("image_view").src = path;
+  var parent = document.getElementById("image_container");
+  var descendants = parent.getElementsByTagName("LI");
+  var i = 0;
+  while (i < descendants.length){
+    if (descendants[i].getElementsByTagName('img')[0].getAttribute("src") == path){
+      descendants[i].style.border = "medium solid white";
+      return;
+    }
+    i++;
+  }
 }
 
 var currFileName;
 var currFilePath;
 var ssPhotoNum;
 var photoIndex;
+var thumbnailIndex;
+var thumbnailSetMaxNumber;
 
 //Be sure to have your file names numbered. Everything has to have the same base file name.
 //You can modify this function to take in a different directory instead.
@@ -17,23 +35,25 @@ function fillImages(filename, filepath, maxphotos, height, width, listID)
 {
   //Clear the list of old pictures
   deleteList();
-  
+
   //Set parameters for the current slideshow
   currFileName = filename;
   currFilePath = filepath;
   ssPhotoNum = maxphotos;
   photoIndex = 1;
-    
+  thumbnailIndex = 1;
+  thumbnailSetMaxNumber = 1;
   var listRef = document.getElementById(listID);
   var bfinished = false;
   var counter = 1;
-    
+
   //View the first image in the gallery
   viewImage(filepath + filename + counter + ".jpg");
   //Load in pictures
   while(!bfinished)
   {
     var new_list_item = document.createElement("li");
+    new_list_item.setAttribute('class', 'imageList');
     var new_img = document.createElement("IMG");
 
     //Set Attributes
@@ -50,6 +70,16 @@ function fillImages(filename, filepath, maxphotos, height, width, listID)
     //Append the list item onto the unordered list
     listRef.appendChild(new_list_item);
     if(++counter == maxphotos) bfinished = true;
+    if (counter % 8 == 0){ //create a new ul if u have more than 7 pictures
+      thumbnailSetMaxNumber++;
+      var divContainer = document.getElementById("image_container");
+      var tempListRef = document.createElement('ul');
+      tempListRef.setAttribute('id', 'image_list' + thumbnailSetMaxNumber);
+      tempListRef.style.display = 'none';
+      divContainer.appendChild(tempListRef);
+      var test = "image_list" + thumbnailSetMaxNumber;
+      listRef = document.getElementById(test);
+    }
   }
 
 }
@@ -74,13 +104,38 @@ function nextImage()
     viewImage(currFilePath + currFileName + photoIndex + ".jpg");
 }
 
-
+function prevThumbnailSet()
+{
+  if (thumbnailIndex - 1  >= 1){
+    document.getElementById("image_list" + thumbnailIndex).style.display = 'none';
+    thumbnailIndex--;
+    document.getElementById("image_list" + thumbnailIndex).style.display = 'inline-block';
+  }
+  else {
+    document.getElementById("image_list" + thumbnailIndex).style.display = 'none';
+    document.getElementById("image_list" + thumbnailSetMaxNumber).style.display = 'inline-block';
+    thumbnailIndex = thumbnailSetMaxNumber;
+  }
+}
+function nextThumbnailSet()
+{
+  if (thumbnailIndex + 1 <= thumbnailSetMaxNumber){
+    document.getElementById("image_list" + thumbnailIndex).style.display = 'none';
+    thumbnailIndex++;
+    document.getElementById("image_list" + thumbnailIndex).style.display = 'inline-block';
+  }
+  else {
+    document.getElementById("image_list" + thumbnailIndex).style.display = 'none';
+    document.getElementById("image_list1").style.display = 'inline-block';
+    thumbnailIndex = 1;
+  }
+}
 
 //Toggles the dropdown content of a section whenever it is clicked.
 //NOTE: For the html the section content NEEDS to have its own id.
 function toggleContent(content)
 {
-    content.classList.toggle("show"); 
+    content.classList.toggle("show");
 }
 
 var content;
