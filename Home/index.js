c7a0c596a62aba13cbca7e85ce3736dcea7e7180
mshen010 <mshen010@ucr.edu>
2017-05-07 16:13:50 -0700

    Autoplay Slideshow Complete
    Some minor strange bug. Beginning two fadeouts flicker.

--- Home/index.js
+++ Home/index.js
@@ -1,24 +1,39 @@
-var slideIndex = 0;
+//var slideIndex = 0;
+//
+//function playSlides() {
+//  "use strict";
+//  var x = document.getElementByClassName("slideshowImg"),
+//    i;
+//  for (i = 0; i < x.length; i += 1) {
+//    x[i].style.display = "none";
+//  }
+//  slideIndex += 1;
+//  if (slideIndex > x.length) {
+//    slideIndex = 1;
+//  }
+//  x[slideIndex - 1].style.display = "block";
+//  setTimeout(playSlides, 2000);
+//}
+//
 //playSlides();
 
-function playSlides() {
-  "use strict";
-  var x = document.getElementByClassName("slideshowImg"),
-    i;
-  for (i = 0; i < x.length; i += 1) {
-    x[i].style.display = "none";
-  }
-  slideIndex += 1;
-  if (slideIndex > x.length) {
-    slideIndex = 1;
-  }
-  x[slideIndex - 1].style.display = "block";
-  setTimeout(playSlides, 2000);
-}
 
-playSlides();
+$(".slideshowContainer > div:gt(0)").hide();
+  
+setInterval(function() { 
+  $('.slideshowContainer > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('.slideshowContainer');
+},  5000);
+
+
+//
 //window.onload = function () {
 //  "use strict";
-//  playSlides();
+////  document.getElementsByClassName("slideshowImg").css('visibility','visible');
+////  playSlides();
 //};
 
