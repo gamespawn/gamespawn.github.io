3b38193f93c8edf6e12822faa3eee8d68135ab20
mshen010 <mshen010@ucr.edu>
2017-09-08 12:49:34 -0700

    Old Changes

--- Home/index.js
+++ Home/index.js
@@ -1,39 +1,22 @@
-//var slideIndex = 0;
-//
-//function playSlides() {
-//  "use strict";
-//  var x = document.getElementByClassName("slideshowImg"),
-//    i;
-//  for (i = 0; i < x.length; i += 1) {
-//    x[i].style.display = "none";
-//  }
-//  slideIndex += 1;
-//  if (slideIndex > x.length) {
-//    slideIndex = 1;
-//  }
-//  x[slideIndex - 1].style.display = "block";
-//  setTimeout(playSlides, 2000);
-//}
-//
-//playSlides();
+var interval = 2000;
+var imageNum = 0;
+var imageArray = [];
 
+$('div').each(function() {
+  var id = $(this).attr('id');
+  $imageArray.push(id);
+});
 
-$(".slideshowContainer > div:gt(0)").hide();
-  
-setInterval(function() { 
+function nextImage() {
+  imageNum = (imageNum+1) % totalImages;
+}
+ 
+function playSlides() { 
+  var currentImage = nextImage();
   $('.slideshowContainer > div:first')
     .fadeOut(1000)
     .next()
     .fadeIn(1000)
-    .end()
-    .appendTo('.slideshowContainer');
+    .end();
+  debugger;
 },  5000);
-
-
-//
-//window.onload = function () {
-//  "use strict";
-////  document.getElementsByClassName("slideshowImg").css('visibility','visible');
-////  playSlides();
-//};
-
