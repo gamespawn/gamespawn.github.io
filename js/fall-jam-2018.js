9f83fc03719441d061c378d49f9c6b60cf358884
Kevin Huang <khuan028@ucr.edu>
2018-11-13 16:22:11 -0800

    Added jam banner and page

---
+++ js/fall-jam-2018.js
@@ -0,0 +1,5 @@
+$(() => {
+    $('#jam-logo').addClass('showing-2');
+    setTimeout(() => $('#event-description').addClass('showing-2'), 100);
+    setTimeout(() => $('#register-button').addClass('showing-2'), 250);
+});
