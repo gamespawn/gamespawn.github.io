2b957c0f6364913ce469d9f99e97f3a5a9875f78
DerpyMissile <hmai015@ucr.edu>
2023-04-26 02:06:07 -0700

    testing new projects homepage

---
+++ js/projects_homepage.js
@@ -0,0 +1,37 @@
+let slides = document.getElementsByClassName("slider__slide");
+let navlinks = document.getElementsByClassName("slider__navlink");
+let currentSlide = 0;
+
+document.getElementById("nav-button--next").addEventListener("click", () => {
+    changeSlide(currentSlide + 1)
+});
+document.getElementById("nav-button--prev").addEventListener("click", () => {
+    changeSlide(currentSlide - 1)
+});
+
+function changeSlide(moveTo) {
+    if (moveTo >= slides.length) {moveTo = 0;}
+    if (moveTo < 0) {moveTo = slides.length - 1;}
+    
+    slides[currentSlide].classList.toggle("active");
+    navlinks[currentSlide].classList.toggle("active");
+    slides[moveTo].classList.toggle("active");
+    navlinks[moveTo].classList.toggle("active");
+    
+    currentSlide = moveTo;
+}
+
+document.querySelectorAll('.slider__navlink').forEach((bullet, bulletIndex) => {
+    bullet.addEventListener('click', () => {
+        if (currentSlide !== bulletIndex) {
+            changeSlide(bulletIndex);
+        }
+    })
+})
+
+document.getElementById("GSprojects").addEventListener("click", () => {
+  window.location.href = "projects.html";
+});
+document.getElementById("GGJprojects").addEventListener("click", () => {
+  window.location.href = "projects.html";
+});
