f00bdfc7a397de901ea8f3f3667b177747f34fb9
DerpyMissile <hmai015@ucr.edu>
2024-07-02 18:56:09 -0700

    made projects page loo better

--- js/projects_homepage.js
+++ js/projects_homepage.js
@@ -29,28 +29,10 @@ function changeDesc(whichProject){
     let projName = document.getElementById("name");
     switch(whichProject){
         case 0:
-            descBox.innerHTML = "Go to the University of California and meet the lovely and dateable mascots across the UC's, take and skip classes to spend time with them, and confess and go to college";
+            descBox.innerHTML = "Summer Timeeeeeeee!!!!!! No projects! Come back in the fall quarter!";
             recBox.innerHTML = "";
             upBox.innerHTML = ""
-            projName.innerHTML = "Cross Campus"
-            break;
-        case 1:
-            descBox.innerHTML = "A game with questionable morals.";
-            recBox.innerHTML = "";
-            upBox.innerHTML = ""
-            projName.innerHTML = "Phantom Thief"
-            break;
-        case 2:
-            descBox.innerHTML = "A puzzle room RPG where you have to navigate through a dungeon, uncover its mysteries, and ultimately escape!";
-            recBox.innerHTML = "";
-            upBox.innerHTML = ""
-            projName.innerHTML = "The Escape Room"
-            break;
-        case 3:
-            descBox.innerHTML = "A game about a delusional main character who thinks they are an AI (they are human). Oh and you can gamble for characters.";
-            recBox.innerHTML = "";
-            upBox.innerHTML = ""
-            projName.innerHTML = "Synthetic Self Symphony"
+            projName.innerHTML = "None"
             break;
     }
 }
@@ -63,15 +45,15 @@ document.querySelectorAll('.slider__navlink').forEach((bullet, bulletIndex) => {
     })
 })
 
-document.getElementById("GSprojects").addEventListener("click", () => {
-  window.location.href = "projectsGS.html";
-});
-document.getElementById("GGJprojects").addEventListener("click", () => {
-  window.location.href = "projectsGGJ.html";
-});
-document.getElementById("Personal").addEventListener("click", () => {
-    window.location.href = "projectsPersonal.html";
-  });
+// document.getElementById("GSprojects").addEventListener("click", () => {
+//   window.location.href = "projectsGS.html";
+// });
+// document.getElementById("GGJprojects").addEventListener("click", () => {
+//   window.location.href = "projectsGGJ.html";
+// });
+// document.getElementById("Personal").addEventListener("click", () => {
+//     window.location.href = "projectsPersonal.html";
+//   });
 
 window.onload = function(){
     changeDesc(currentSlide);
